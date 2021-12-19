970a965264620853591e04634767a67a356de58e
Mohammad Zafar <mzcoder7@gmail.com>
2021-12-19 18:31:58 +0000

    remove icon at 4 sec interval

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const icons = document.querySelectorAll(".section-1-icons i");
+
+setInterval(() => {
+  const icon = document.querySelector(".section-1-icons.change");
+  icon.classList.remove("change");
+}, 4000);
