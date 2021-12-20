6cece63305c0570f54619a45e908e5737f5e917c
Mohammad Zafar <mzcoder7@gmail.com>
2021-12-20 13:46:59 +0000

    check for typos + format text

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
+// on click event for menu icons
 document.querySelector(".menu").addEventListener("click", () => {
   document.querySelectorAll(".target").forEach((item) => {
     item.classList.toggle("change");
   });
 });
-
+// go through work icons in sequence
+// start with briefcase at 4 sec intervals
 const icons = document.querySelectorAll(".section-1-icons i");
 let i = 1;
 
 setInterval(() => {
   i++;
-  const icon = document.querySelector(".section-1-icons.change");
+  const icon = document.querySelector(".section-1-icons .change");
   icon.classList.remove("change");
+
   if (i > icons.length) {
     icons[0].classList.add("change");
     i = 1;
